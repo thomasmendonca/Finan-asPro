c0e9743e55a996cc044b677b953b9509f6313f66
joaocarloslima <joaocarloslima@me.com>
2024-03-04 20:54:31 -0300

    add post and get categoria

--- src/main/java/br/com/fiap/financaspro/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/financaspro/controller/CategoriaController.java
@@ -1,32 +1,62 @@
 package br.com.fiap.financaspro.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import br.com.fiap.financaspro.model.Categoria;
+
+@RestController
+@RequestMapping("categoria")
 public class CategoriaController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/categoria", produces = "application/json")
-    @ResponseBody
-    public String index() {
-        return """
+    Logger log = LoggerFactory.getLogger(getClass());
 
-                [
-                    {
-                        "id": 1,
-                        "nome": "Alimentação",
-                        "icone": "fast-food"
-                    },
-                    {
-                        "id": 2,
-                        "nome": "Educação",
-                        "icone": "book"
-                    }
-                    ]
-                """;
+    List<Categoria> repository = new ArrayList<>();
+
+    @GetMapping
+    public List<Categoria> index() {
+        return repository;
 
     }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Categoria create(@RequestBody Categoria categoria){ //binding
+        log.info("cadastrando categoria {} ", categoria);
+        repository.add(categoria);
+        return categoria;
+    }
+    
+    @GetMapping("{id}")
+    public ResponseEntity<Categoria> show(@PathVariable Long id){
+        log.info("buscando categoria por id {}", id);
+
+        for(Categoria categoria : repository){
+            if (categoria.id().equals(id)) 
+                return ResponseEntity.ok(categoria);
+        }
+
+        // TODO Refatorar com stream
+
+        return ResponseEntity.notFound().build();
+
+    }
+
+
 
 }
