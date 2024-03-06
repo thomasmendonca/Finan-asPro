1f6f79b05a2b848a677b0832d696cc036aa7dadd
joaocarloslima <joaocarloslima@me.com>
2024-03-05 22:48:12 -0300

    add delete and put

--- src/main/java/br/com/fiap/financaspro/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/financaspro/controller/CategoriaController.java
@@ -2,19 +2,19 @@ package br.com.fiap.financaspro.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,27 +36,77 @@ public class CategoriaController {
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Categoria create(@RequestBody Categoria categoria){ //binding
+    public Categoria create(@RequestBody Categoria categoria) { // binding
         log.info("cadastrando categoria {} ", categoria);
         repository.add(categoria);
         return categoria;
     }
-    
+
     @GetMapping("{id}")
-    public ResponseEntity<Categoria> show(@PathVariable Long id){
+    public ResponseEntity<Categoria> show(@PathVariable Long id) {
         log.info("buscando categoria por id {}", id);
 
-        for(Categoria categoria : repository){
-            if (categoria.id().equals(id)) 
-                return ResponseEntity.ok(categoria);
+        // for(Categoria categoria : repository){
+        // if (categoria.id().equals(id))
+        // return ResponseEntity.ok(categoria);
+        // }
+
+        var categoriaEncontrada = getCategoriaById(id);
+
+        if (categoriaEncontrada.isEmpty()) {
+            return ResponseEntity.notFound().build();
         }
 
-        // TODO Refatorar com stream
+        return ResponseEntity.ok(categoriaEncontrada.get());
 
-        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object> destroy(@PathVariable Long id) {
+        log.info("apagando categoria");
+        var categoriaEncontrada = getCategoriaById(id);
+
+        if (categoriaEncontrada.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        repository.remove(categoriaEncontrada.get());
+        return ResponseEntity.noContent().build();
 
     }
 
 
+    @PutMapping("{id}")
+    public ResponseEntity<Categoria> update(@PathVariable Long id, @RequestBody Categoria categoria){
+        log.info("atualizando categoria com id {} para {}", id, categoria);
+        // buscar a categoria
+        var categoriaEncontrada = getCategoriaById(id);
+
+        if (categoriaEncontrada.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        // criar uma nova categoria com os novos dados
+        var categoriaAntiga = categoriaEncontrada.get();
+        var categoriaNova = new Categoria(id, categoria.nome(), categoria.icone());
+        
+        // apagar a categoria antiga
+        repository.remove(categoriaAntiga);
+
+        // add a categoria nova
+        repository.add(categoriaNova);
+
+        return ResponseEntity.ok(categoriaNova);
+    }
+
+
+
+
+
+    private Optional<Categoria> getCategoriaById(Long id) {
+        var categoriaEncontrada = repository
+                .stream()
+                .filter(c -> c.id().equals(id))
+                .findFirst();
+        return categoriaEncontrada;
+    }
 
 }
