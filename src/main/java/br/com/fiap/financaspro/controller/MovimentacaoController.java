f76564cd2b495adbd02ae92e88c6cb46431fd289
joaocarloslima <joaocarloslima@me.com>
2024-03-19 22:46:01 -0300

    bean validation

--- src/main/java/br/com/fiap/financaspro/controller/MovimentacaoController.java
+++ src/main/java/br/com/fiap/financaspro/controller/MovimentacaoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.financaspro.model.Movimentacao;
 import br.com.fiap.financaspro.repository.MovimentacaoRepository;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("movimentacao")
@@ -29,7 +30,7 @@ public class MovimentacaoController {
 
     @PostMapping
     @ResponseStatus(CREATED)
-    public Movimentacao create(@RequestBody Movimentacao movimentacao){
+    public Movimentacao create(@RequestBody @Valid Movimentacao movimentacao){
         return repository.save(movimentacao);
     }
 
